1b6d85b98ce5c3c3ba6d1d5bf519ffc835588f5d
ron0401 <51016256+ron0401@users.noreply.github.com>
2020-11-14 14:08:31 +0900

    init

---
+++ sttnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sttnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
