40a92592c694c44192fd9d8008bde9ec7be0911a
ron0401 <51016256+ron0401@users.noreply.github.com>
2020-11-15 21:29:56 +0900

    add send udp

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -3,24 +3,29 @@
 #include <M5StickC.h>
 
 
-#define BAUD_RATE   9600
-#define MYSSID      "ssid"
-#define MYPASSWD    "pass"
-#define LISTEN_PORT 50000
+#define BAUD_RATE     9600
+#define MYSSID        "ssid"
+#define MYPASSWD      "pass"
+#define LISTEN_PORT   (int)50000
+#define SEND_BUF_SIZE 1024
 
+#define NODATA        -1
 
-const char * ssid = MYSSID;
-const char * password = MYPASSWD;
+const char*         ssid = MYSSID;
+const char*         password = MYPASSWD;
+static const char*  RemoteIp = "***.***.***.**";
+static const int    RemoteUdpPort = 50000;
 
-uint8_t * readBuffer;
+uint8_t*            readBuffer;
+uint8_t             sendBuffer[SEND_BUF_SIZE];
+unsigned int        buf_counter = 0;               
 
 AsyncUDP udp;
 
 void setup() {
   // put your setup code here, to run once:
+  buf_clear();
   M5.begin();
-  M5.Lcd.setRotation(3);
-  M5.Lcd.fillScreen(BLACK);
   Serial.begin(BAUD_RATE);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -39,5 +44,31 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int buf;
+  buf = Serial.read();
+  if (buf == NODATA)
+  {
+    if (buf_counter != 0)
+    {
+      udp.write(sendBuffer, buf_counter);
+      buf_clear();
+    }
+  }
+  else
+  {
+    sendBuffer[buf_counter] = (uint8_t)buf;
+    buf_counter++;
+    if (buf_counter >= (SEND_BUF_SIZE - 1))
+    {
+      udp.write(sendBuffer, buf_counter);
+      buf_clear();
+    }    
+  }
+}
 
+void buf_clear() {
+  for (int i = 0; i < SEND_BUF_SIZE; i++)
+  {
+    sendBuffer[i] = 0;
+  }      
 }
